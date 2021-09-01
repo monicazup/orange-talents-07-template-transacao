251054b96a0601dfa75ef06282afb207daf879fd
monicazup <monica.gomides@zup.com.br>
2021-09-01 15:36:20 -0300

    Setup do projeto

---
+++ src/main/java/com/zupedu/monica/transacoesot7/TransacoesOt7Application.java
@@ -0,0 +1,13 @@
+package com.zupedu.monica.transacoesot7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacoesOt7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacoesOt7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zupedu/monica/transacoesot7/TransacoesOt7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupedu.monica.transacoesot7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesOt7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
